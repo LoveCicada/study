c52599fe2e0822a4ee59b590ca27bd65da8b63d3
loveCicada <loveCicada.com>
2021-06-28 12:46:51 +0800

    add lambda connect siganl and signal

--- signalConnectSignal/widget.h
+++ signalConnectSignal/widget.h
@@ -15,11 +15,17 @@ public:
 		:QPushButton(str,parent)
 	{
 		connect(this, &MyPushButton::clicked, this, &MyPushButton::mySignal);
+		connect(this, &MyPushButton::clicked, [this](bool checked = false) {
+			emit mySignal2(checked);
+		}
+		);
 		connect(this, &MyPushButton::mySignal, this, &MyPushButton::testMySignal);
+		connect(this, &MyPushButton::mySignal2, this, &MyPushButton::testMySignal2);
 	}
 
 signals:
 	void mySignal(bool checked = false);
+	void mySignal2(bool checked = false);
 
 public slots:
 	void testMySignal(bool checked = false)
@@ -27,6 +33,10 @@ public slots:
 		qDebug() << "test my signal";
 	}
 
+	void testMySignal2(bool checked = false)
+	{
+		qDebug() << "test my signal2";
+	}
 
 };
 
