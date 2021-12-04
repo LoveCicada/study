eefff4d86f2e583fa4e54f208b7cf13b8de5eeb1
loveCicada <loveCicada@qq.com>
2021-12-04 21:18:32 +0800

    variadic type

--- C++/C++11/varibaleTemplateParam/simple/build.sh
+++ C++/C++11/varibaleTemplateParam/simple/build.sh
@@ -15,4 +15,8 @@ g++ -o ./bin/VTPrint VTPrint.cpp -std=c++11
 echo "complex_variable_template"
 g++ -o ./bin/complex_variable_template complex_variable_template.cpp -std=c++11
 
+echo "multi_template_params"
+g++ -o ./bin/multi_template_params multi_template_params.cpp -std=c++11
 
+echo "variadicType_forward"
+g++ -o ./bin/variadicType_forward variadicType_forward.cpp -std=c++11
