c58a3ea33442d822f88b1e7406043c35bce48a8a
loveCicada <loveCicada@qq.com>
2022-03-12 10:11:19 +0800

    stringstream study

--- C++/basic/fundament/build.sh
+++ C++/basic/fundament/build.sh
@@ -1,6 +1,7 @@
 #!/bin/bash
 
 echo "string2wstring"
-
 g++ -o ./bin/string2wstring string2wstring.cpp
 
+echo "stringstream_study"
+g++ -o ./bin/stringstream_study stringstream_study.cpp
---
+++ C++/basic/fundament/stringstream_study.cpp
@@ -0,0 +1,48 @@
+#include <iostream>
+#include <sstream>
+using namespace std;
+
+#define STRING_TYPE(c1, c2, c3, c4)                 \
+        ( (static_cast<uint32_t>(c1))<<24 )     |   \
+        ( (static_cast<uint32_t>(c2))<<16 )     |   \
+        ( (static_cast<uint32_t>(c3))<<8 )      |   \
+        ( (static_cast<uint32_t>(c4)) )     
+
+const uint32_t uiftyp = STRING_TYPE('f','t','y','p'); 
+
+uint32_t& string2uint32_t(char c1, char c2, char c3, char c4, uint32_t& v)
+{
+    v = ( (static_cast<uint32_t>(c1))<<24 )     |   \
+        ( (static_cast<uint32_t>(c2))<<16 )     |   \
+        ( (static_cast<uint32_t>(c3))<<8 )      |   \
+        ( (static_cast<uint32_t>(c4)) );
+
+     return v;   
+}
+
+stringstream& print_type(stringstream& ss, uint32_t v)
+{
+    ss << char(v>>24) << char(v>>16) << char(v>>8) << char(v);
+    return ss;
+}
+
+int main(int argc, char* argv[])
+{
+    stringstream ss;
+    print_type(ss, uiftyp);
+    cout << "uiftyp: " << uiftyp << endl;
+    cout << "ss: " << ss.str().c_str() << endl;
+
+    uint32_t v;
+    string2uint32_t('f', 't', 'y', 'p', v);
+    
+    //! 清空流状态
+    ss.clear();
+    //! 清空流内容
+    ss.str("");
+    print_type(ss, v);
+    cout << "v: " << v << endl;
+    cout << "ss: " << ss.str().c_str() << endl;
+
+    return 0;
+}
