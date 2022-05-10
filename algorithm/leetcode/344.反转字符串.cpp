08e2526977281dd0567aaa56e10fa74b63b5373f
loveCicada <loveCicada@qq.com>
2022-05-10 21:49:25 +0800

    反转字符串

--- algorithm/leetcode/344.反转字符串.cpp
+++ algorithm/leetcode/344.反转字符串.cpp
@@ -8,7 +8,11 @@
 class Solution {
 public:
     void reverseString(vector<char>& s) {
-
+        for (size_t i = 0, j = s.size() - 1; i < s.size() / 2; i++, j--) {
+            char c = s[i];
+            s[i] = s[j];
+            s[j] = c;
+        }
     }
 };
 // @lc code=end
