b01098a17f710ca1e6b7e88e881707695625ebce
loveCicada <loveCicada@qq.com>
2022-06-12 23:04:38 +0800

    单例模板类注释

--- designMode/singleton/singleton.cpp
+++ designMode/singleton/singleton.cpp
@@ -17,7 +17,7 @@ using std::endl;
 template<typename T>
 class Singleton
 {
-    friend T;
+    friend T;   //T can visit Singleton private member and function
 public:
     static T* GetInstance() { 
         static T instance;
@@ -40,7 +40,7 @@ private:
 
 class SingletonDerived : public Singleton<SingletonDerived>
 {
-    friend Singleton;
+    friend Singleton;   //Singleton can visit SingletonDerived private member and function
 private:
     SingletonDerived(){
         cout << __func__ << endl;
@@ -65,5 +65,34 @@ int main(int argc, char* argv[]) {
     SingletonDerived* pSd2 = SingletonDerived::GetInstance();
     pSd2->print();
 
+    if(pSd == pSd2) {
+        cout << "the same object." << endl;
+    }
+
     return 0;
 }
+
+/**
+ * @result 
+ *  Singleton
+    SingletonDerived
+    print this: 0x55e3bca0c152
+    print this: 0x55e3bca0c152
+    the same object.
+    ~SingletonDerived
+    ~Singleton
+ */
+
+/**
+ * @brief 
+ *  例如，以下语句说明类B是类A的友元类：
+    class A
+    {
+    …
+    public:
+        friend class B;
+    …
+    };
+    经过以上说明后，类B的所有成员函数都是类A的友元函数，
+    能存取类A的私有成员和保护成员。
+ */
