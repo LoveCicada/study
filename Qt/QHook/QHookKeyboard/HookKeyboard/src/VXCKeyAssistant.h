51e2704ab43e1586afec5d484d9d773e625130a1
duanyafeng <duanyafeng@sobey.com>
2022-08-21 12:59:01 +0800

    增加CMakeLists.txt工程构建文件

---
+++ Qt/QHook/QHookKeyboard/HookKeyboard/src/VXCKeyAssistant.h
@@ -0,0 +1,33 @@
+﻿
+#pragma once
+
+/*
+@brief
+*/
+#include <functional>
+
+class CKeyAssistant
+{
+public:
+    typedef std::function<void(void* cmdData)> CallBack;
+
+public:
+    virtual ~CKeyAssistant() {}
+
+public:
+    CKeyAssistant();
+    void Init();
+
+    //! set callback
+    void SetCallBack(CallBack cb);
+
+public:
+    virtual void Process() = 0;
+    virtual void SetHook() = 0;
+    virtual void UnSetHook() = 0;
+    virtual void Active() = 0;
+    virtual void UnActive() = 0;
+
+protected:
+    CallBack m_pCb;
+};
