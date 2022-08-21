fcede712bb2ffca124e7bb80eeaccfa348a45507
duanyafeng <duanyafeng@sobey.com>
2022-08-21 12:25:07 +0800

    记录键盘按键工具，Qt::key 与 Windows Hook 日志：keyevent.log

---
+++ Qt/QHook/QHookKeyboard/QHookKeyboard/widget.h
@@ -0,0 +1,27 @@
+#ifndef WIDGET_H
+#define WIDGET_H
+
+#include <QWidget>
+#include "VXCKeyAssistantFactory.h"
+
+class Widget : public QWidget
+{
+    Q_OBJECT
+
+private:
+    typedef unique_ptr<CKeyAssistant> CKeyAssistantPtr;
+
+public:
+    Widget(QWidget *parent = 0);
+    ~Widget();
+protected:
+    virtual void keyPressEvent(QKeyEvent* e) override;
+    virtual void keyReleaseEvent(QKeyEvent* e) override;
+    virtual void focusInEvent(QFocusEvent* e) override;
+    virtual void focusOutEvent(QFocusEvent* e) override;
+
+private:
+    CKeyAssistantPtr m_pCKeyAssistant;
+};
+
+#endif // WIDGET_H
---
+++ Qt/QHook/QHookKeyboard/QHookKeyboard/win/VXCKeyAssistantWin.h
@@ -0,0 +1,27 @@
+﻿
+#pragma once
+
+#include "../VXCKeyAssistant.h"
+/*
+@brief
+*/
+
+class CKeyAssistantWin final : public CKeyAssistant
+{
+public:
+    CKeyAssistantWin();
+    ~CKeyAssistantWin();
+    void InitHanlde();
+
+public:
+    virtual void Process() override;
+
+    /*
+     * @brief SetHook() <--> UnSetHook()
+     *           1:1
+     */
+    virtual void SetHook() override;
+    virtual void UnSetHook() override;
+    virtual void Active() override;
+    virtual void UnActive() override;
+};
