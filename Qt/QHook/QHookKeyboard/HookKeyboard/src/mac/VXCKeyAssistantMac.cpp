73907a662160632a8190d244c0dc9c58b4b0f5c8
duanyafeng <duanyafeng@sobey.com>
2023-01-30 14:43:14 +0800

    keyboard tool adapt mac

---
+++ Qt/QHook/QHookKeyboard/HookKeyboard/src/mac/VXCKeyAssistantMac.cpp
@@ -0,0 +1,50 @@
+﻿
+#if defined(__APPLE__) && defined(__MACH__)
+
+#include "VXCKeyAssistantMac.h"
+#include "../log/VXLog.h"
+#include <iostream>
+#include <sstream>
+using namespace std;
+
+
+CKeyAssistantMac::CKeyAssistantMac()
+{
+    InitHanlde();
+    SetHook();
+    Active();
+}
+
+CKeyAssistantMac::~CKeyAssistantMac()
+{
+    UnSetHook();
+    UnActive();
+}
+
+void CKeyAssistantMac::InitHanlde()
+{
+}
+
+void CKeyAssistantMac::Process() {}
+
+void CKeyAssistantMac::SetHook()
+{
+    //SetHookEx();
+}
+
+void CKeyAssistantMac::UnSetHook()
+{
+    //UnSetHookEx();
+}
+
+void CKeyAssistantMac::Active()
+{
+    //ActiveEx();
+}
+
+void CKeyAssistantMac::UnActive()
+{
+    //UnActiveEx();
+}
+
+#endif
